2cef1c3fccf307cb5f461e56b9c96d671a4c80bc
sungyotkim <sungyotkim@gmail.com>
2022-10-22 16:21:04 -0400

    add cors

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,9 @@ const express = require("express");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
+const cors = require("cors");
+const { isProduction } = require("./config/keys");
+
 const usersRouter = require("./routes/api/users");
 const tweetsRouter = require("./routes/api/tweets");
 
@@ -13,7 +15,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
-app.use("/", indexRouter);
+if (!isProduction) {
+  app.use(cors());
+}
+
 app.use("/api/users", usersRouter);
 app.use("/api/tweets", tweetsRouter);
 
