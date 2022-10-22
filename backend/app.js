d1f1c7298f73278368dd2d372a9f3c304267d32c
sungyotkim <sungyotkim@gmail.com>
2022-10-22 16:19:38 -0400

    remove associated code from app

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
@@ -13,7 +12,6 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/api/users", usersRouter);
