7008100e798a98d2a02389577c1cfb24b910eb48
sungyotkim <sungyotkim@gmail.com>
2022-10-22 20:16:32 -0400

    add navbar

---
+++ frontend/components/NavBar/NavBar.js
@@ -0,0 +1,41 @@
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../store/session";
+
+const NavBar = () => {
+  const loggedIn = useSelector((state) => !!state.session.user);
+  const dispatch = useDispatch();
+
+  const logoutUser = (e) => {
+    e.preventDefault();
+    dispatch(logout());
+  };
+
+  const getLinks = () => {
+    if (loggedIn) {
+      return (
+        <div className="links-nav">
+          <Link to={"/tweets"}>All Tweets</Link>
+          <Link to={"/profile"}>Profile</Link>
+          <Link to={"/tweets/new"}>Write a Tweet</Link>
+          <button onClick={logoutUser}>Logout</button>
+        </div>
+      );
+    } else {
+      return (
+        <div className="links-auth">
+          <Link to={"/signup"}>Signup</Link>
+          <Link to={"/login"}>Login</Link>
+        </div>
+      );
+    }
+  };
+
+  return (
+    <>
+      <h1>Chirper</h1>
+      {getLinks()}
+    </>
+  );
+};
+
+export default NavBar;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,20 @@
 import { Switch } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "./components/Routes/Routes";
-
 import MainPage from "./components/MainPage/MainPage";
+import NavBar from "./components/NavBar/NavBar";
+import LoginForm from "./components/SessionForms/LoginForm";
+import SignupForm from "./components/SessionForms/SignupForm";
 
 function App() {
   return (
-    <Switch>
-      <AuthRoute exact path="/" component={MainPage} />
-    </Switch>
+    <>
+      <NavBar />
+      <Switch>
+        <AuthRoute exact path="/" component={MainPage} />
+        <AuthRoute exact path="/login" component={LoginForm} />
+        <AuthRoute exact path="/signup" component={SignupForm} />
+      </Switch>
+    </>
   );
 }
 
