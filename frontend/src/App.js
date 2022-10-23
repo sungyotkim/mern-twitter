f3de1274d631b5b3bde87aa3095ccedd444ceb42
sungyotkim <sungyotkim@gmail.com>
2022-10-22 21:30:54 -0400

    add tweets

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,10 @@ import MainPage from "./components/MainPage/MainPage";
 import NavBar from "./components/NavBar/NavBar";
 import LoginForm from "./components/SessionForms/LoginForm";
 import SignupForm from "./components/SessionForms/SignupForm";
+import Tweets from "./components/Tweets/Tweets";
+import Profile from "./components/Profile/Profile";
+import TweetCompose from "./components/Tweets/TweetCompose";
+
 import { getCurrentUser } from "./store/session";
 
 function App() {
@@ -21,6 +25,10 @@ function App() {
           <AuthRoute exact path="/" component={MainPage} />
           <AuthRoute exact path="/login" component={LoginForm} />
           <AuthRoute exact path="/signup" component={SignupForm} />
+
+          <ProtectedRoute exact path="/tweets" component={Tweets} />
+          <ProtectedRoute exact path="/profile" component={Profile} />
+          <ProtectedRoute exact path="/tweets/new" component={TweetCompose} />
         </Switch>
       </>
     )
---
+++ frontend/src/components/Tweets/TweetBox.js
@@ -0,0 +1,13 @@
+const TweetBos = ({ text, username }) => {
+  return (
+    <>
+      <div className="tweet">
+        <h3>
+          {username ? `${username}:` : ""} {text}{" "}
+        </h3>
+      </div>
+    </>
+  );
+};
+
+export default TweetBos;
