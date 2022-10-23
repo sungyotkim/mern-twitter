908140ceae8e0014f50ed512f80600cc0ebd927a
sungyotkim <sungyotkim@gmail.com>
2022-10-22 20:20:22 -0400

    add store functions

---
+++ frontend/store/session.js
@@ -0,0 +1,58 @@
+import { startSession } from "../../backend/models/Tweet";
+import jwtFetch from "./jwt";
+
+const RECEIVE_CURRENT_USER = "session/RECEIVE_CURRENT_USER";
+const RECEIVE_SESSION_ERRORS = "session/RECEIVE_SESSION_ERRORS";
+const CLEAR_SESSION_ERRORS = "session/CLEAR_SESSION_ERRORS";
+export const RECEIVE_USER_LOGOUT = "session/RECEIVE_USER_LOGOUT";
+
+// Dispatch receiveCurrentUser when a user logs in
+const receiveCurrentUser = (currentUser) => ({
+  type: RECEIVE_CURRENT_USER,
+  currentUser,
+});
+
+// Dispatch receiveErrors to show authentication errors on the frontend.
+const receiveErrors = (errors) => ({
+  type: RECEIVE_SESSION_ERRORS,
+  errors,
+});
+
+// Dispatch logoutUser to clear the session user when a user logs out.
+const logoutUser = () => ({
+  type: RECEIVE_USER_LOGOUT,
+});
+
+// Dispatch clearSessionErrors to clear any session errors.
+export const clearSessionErrors = () => ({
+  type: CLEAR_SESSION_ERRORS,
+});
+
+//Because signup also logs in the newly created user,
+// login and signup essentially differ only in their route and user information sent.
+// You should accordingly be able to abstract their common elements into a helper function.
+
+export const signup = (user) => startSession(user, "api/users/register");
+export const login = (user) => startSession(user, "api/users/login");
+
+const startSession = (userInfo, route) => async (dispatch) => {
+  try {
+    const res = await jwtFetch(route, {
+      method: "POST",
+      body: JSON.stringify(userInfo),
+    });
+    const { user, token } = await res.json();
+    localStorage.setItem("jwtToken", token);
+    return dispatch(receiveCurrentUser(user));
+  } catch (err) {
+    const res = await err.json();
+    if (res.statusCode === 400) {
+      return dispatch(receiveErrors(res.errors));
+    }
+  }
+};
+
+export const logout = () => (dispatch) => {
+  localStorage.removeItem("jwtToken");
+  dispatch(logoutUser());
+};
